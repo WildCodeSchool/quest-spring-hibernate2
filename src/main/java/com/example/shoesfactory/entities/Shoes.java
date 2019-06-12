242c0cbe4af99da2320652a1cdf65ec07f2ecc41
bastienwcs <email>
2019-06-12 16:00:35 +0200

    init

---
+++ src/main/java/com/example/shoesfactory/entities/Shoes.java
@@ -0,0 +1,50 @@
+package com.example.shoesfactory.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Shoes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @NotNull
+    private String model;
+
+    @NotNull
+    private Double price;
+
+    public Shoes() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
